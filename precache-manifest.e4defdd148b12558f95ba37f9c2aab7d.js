0028df66fff8de9ecc1ddf9d4da1b1e1ddfbb1da
Monse0051 <rouge0051@gmail.com>
2019-05-27 17:51:53 -0500

    Updates

---
+++ precache-manifest.e4defdd148b12558f95ba37f9c2aab7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbe98975c8787a77e212cf2f8815fba",
+    "url": "/GDL002-burguer-queen/my-app/index.html"
+  },
+  {
+    "revision": "83646d92e421e590bf0c",
+    "url": "/GDL002-burguer-queen/my-app/static/css/main.06fcdcb9.chunk.css"
+  },
+  {
+    "revision": "d4e5702bf51049d0b9dc",
+    "url": "/GDL002-burguer-queen/my-app/static/js/2.6eb5c426.chunk.js"
+  },
+  {
+    "revision": "83646d92e421e590bf0c",
+    "url": "/GDL002-burguer-queen/my-app/static/js/main.474ecb68.chunk.js"
+  },
+  {
+    "revision": "6b84405a4c8504b828f2",
+    "url": "/GDL002-burguer-queen/my-app/static/js/runtime~main.03941d2d.js"
+  }
+]);
